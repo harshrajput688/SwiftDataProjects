e169691317196151980ac0408fa786f94ba6f246
Harsh Rajput <77964116+harshrajput688@users.noreply.github.com>
2025-06-24 13:23:48 +0530

    added new project MyBooks

---
+++ MyBooks/MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Harsh Rajput on 24/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyBooks/MyBooksTests/MyBooksTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksTests.swift
+//  MyBooksTests
+//
+//  Created by Harsh Rajput on 24/06/25.
+//
+
+import Testing
+@testable import MyBooks
+
+struct MyBooksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
